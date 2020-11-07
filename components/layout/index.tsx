f6ee8f543914f489c1c2200948ddee4e5b465a7b
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-11-08 05:33:33 +0800

    update layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,6 @@
+import { Container } from '@chakra-ui/core';
 import React from 'react';
 
 export const Layout: React.FC = ({ children }) => {
-  return <>{children}</>;
+  return <Container maxW="420px">{children}</Container>;
 };
