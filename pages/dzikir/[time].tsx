205a8c96d2ce0dd9ec2d4a6e7ee5b32816d17e9f
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-11-09 04:57:00 +0800

    update dzikir page

--- pages/dzikir/[time].tsx
+++ pages/dzikir/[time].tsx
@@ -12,45 +12,44 @@ const Index: NextPage = () => {
       <NextSeo title="Dzikrr" description="Aplikasi Dzikir Pagi dan Petang" />
 
       <Box pos="relative">
-        {data &&
-          data.map((item) => (
-            <Box
-              _even={{ bgColor: 'rgba(251, 240, 218, 0.24)' }}
+        {data?.map((item) => (
+          <Box
+            _even={{ bgColor: 'rgba(251, 240, 218, 0.24)' }}
+            borderBottom="1px solid rgba(0, 0, 0, 0.1)"
+            key={item.id}
+          >
+            <Flex
+              justify="space-between"
               borderBottom="1px solid rgba(0, 0, 0, 0.1)"
-              key={item.id}
+              p={4}
+              align="baseline"
             >
-              <Flex
-                justify="space-between"
-                borderBottom="1px solid rgba(0, 0, 0, 0.1)"
-                p={4}
-                align="baseline"
+              <Text fontWeight="600">{item.data.title}</Text>
+              <Text fontSize="sm" opacity={0.7}>
+                {item.data.note}
+              </Text>
+            </Flex>
+            <Box p={4}>
+              <Box
+                textAlign="right"
+                fontFamily="'Scheherazade', serif"
+                fontWeight="700"
+                fontSize="3xl"
+                mb={4}
               >
-                <Text fontWeight="600">{item.data.title}</Text>
-                <Text fontSize="sm" opacity={0.7}>
-                  {item.data.note}
-                </Text>
-              </Flex>
-              <Box p={4}>
-                <Box
-                  textAlign="right"
-                  fontFamily="'Scheherazade', serif"
-                  fontWeight="700"
-                  fontSize="3xl"
-                  mb={4}
-                >
-                  {item.data.arabic}
-                </Box>
-                {localStorage.getItem('display_arabicLatin') && (
-                  <Text fontStyle="italic" mb={4}>
-                    {item.data.arabic_latin}
-                  </Text>
-                )}
-                {localStorage.getItem('display_translatedId') && (
-                  <Text>{item.data.translated_id}</Text>
-                )}
+                {item.data.arabic}
               </Box>
+              {localStorage.getItem('display_arabicLatin') && (
+                <Text fontStyle="italic" mb={4}>
+                  {item.data.arabic_latin}
+                </Text>
+              )}
+              {localStorage.getItem('display_translatedId') && (
+                <Text>{item.data.translated_id}</Text>
+              )}
             </Box>
-          ))}
+          </Box>
+        ))}
       </Box>
     </>
   );
