4d7c7bc613b307d53c026ecc38de2f7df8041761
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-11-09 03:17:17 +0800

    firebase env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ const path = require('path');
 
 const nextConfig = {
   // your config
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  },
 
   // webpack config
   webpack(config, _options) {
