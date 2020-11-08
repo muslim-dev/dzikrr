6bfa23d19dbe8bb833d8f472a9777fda0b2ec6ea
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-11-09 01:19:41 +0800

    rename component swipeablePanel to navigation

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/core';
 import React from 'react';
 
-export const SwipeablePanel: React.FC = ({ children }) => {
+export const Navigation: React.FC = ({ children }) => {
   return (
     <Box
       backgroundColor="primary"
@@ -9,6 +9,7 @@ export const SwipeablePanel: React.FC = ({ children }) => {
       w="100%"
       maxW="xl"
       p={6}
+      pb={10}
       borderTopRadius="40px"
       left="50%"
       bottom="0"
