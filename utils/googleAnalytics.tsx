00b470939bd0d4a5335ef375f00a62d90cef5cd4
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2020-11-12 06:07:32 +0800

    new utils for google analytics

---
+++ utils/googleAnalytics.tsx
@@ -0,0 +1,19 @@
+import ReactGA from 'react-ga';
+
+export const gaInit = () => {
+  const googleKey: string = process.env.API_GOOGLE_ANALYTIC_KEY || '';
+  ReactGA.initialize(googleKey, { debug: true });
+};
+
+export const gaLogPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
+export const gaLogEvent = (category: string, action: string) => {
+  ReactGA.event({ category, action });
+};
+
+export const gaLogException = (description: string, fatal = false) => {
+  ReactGA.exception({ description, fatal });
+};
