fa6992d79d0f09f004f3902875424f9555fb72fa
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-01-30 10:17:21 +0800

    add intial scale

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,6 +29,10 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/icon-192.png" />
           <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <meta name="theme-color" content="#FFFFFF" />
         </Head>
         <body>
